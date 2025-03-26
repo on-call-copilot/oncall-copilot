b2948cd2b0dac29dec1aa26f5e215e5660ff647e
Jatin Goel <jgoel@rippling.com>
2025-03-26 16:35:19 +0530

    clean up redundant files

--- calculator/utils.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="A simple command-line calculator.")
-    parser.add_argument("--a", type=float, required=True, help="First number")
-    parser.add_argument("--b", type=float, required=True, help="Second number")
-    parser.add_argument("--op", type=str, required=True, choices=["add", "subtract", "multiply", "divide"], help="Operation to perform")
-    return parser.parse_args()
-
